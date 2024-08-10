055bf2f2e819550e565d2ea3b99b25ec13a97cd1
rayves <vo.raymond.v@outlook.com>
2024-08-10 21:26:54 +1000

    adds state for dice and refactors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import './App.scss';
+import { useState } from 'react';
 import Die from './components/Die';
 import { generateRandomNumber } from './common/helperMethods';
 
 function App() {
-  const dies = [...Array(10).keys()].map((num) => {
-    return <Die key={num} value={generateRandomNumber(6)} />;
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice(): number[] {
+    return Array.from({ length: 10 }, () => generateRandomNumber(6));
+  }
+
+  const diceElements: JSX.Element[] = dice.map((die) => {
+    return <Die value={die} />;
   });
+
   return (
     <main>
       <div className="game-container">
         <header>
           <h1>Tenzies</h1>
         </header>
-        <div className="die-container">{dies}</div>
+        <div className="die-container">{diceElements}</div>
       </div>
     </main>
   );
